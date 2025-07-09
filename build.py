157cc1a8633ff7923af423e4745f3eeffa3554ee
Zach Strout <43109787+RTnhN@users.noreply.github.com>
2025-07-09 13:06:59 -0500

    fix: add message about precompiled libs

--- build.py
+++ build.py
@@ -2,4 +2,7 @@ from gaitmap.trajectory_reconstruction.trajectory_methods import (
     compiled_kalman_funcs,
 )
 
-compiled_kalman_funcs.cc.compile()
+try:
+    compiled_kalman_funcs.cc.compile()
+except AttributeError:
+    print("The compiled Kalman funcs have already been compiled.")  # noqa: T201
